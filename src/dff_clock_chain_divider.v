b455793f5aa6ffa97332f5305422f7e84155e0dc
Bindu Sree Chandu <87289560+Bindu987@users.noreply.github.com>
2025-06-13 06:26:50 -0700

    Update dff_clock_chain_divider.v

--- src/dff_clock_chain_divider.v
+++ src/dff_clock_chain_divider.v
@@ -1,1 +1,19 @@
 
+module dff_clock_chain_divider (
+    input clk,
+    input rst,
+    output [7:0] out
+);
+
+    reg [7:0] dc;
+
+    assign out = dc;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst)
+            dc <= 8'b00000000;
+        else
+            dc <= {dc[6:0], ~dc[7]};  // Shift + toggle MSB
+    end
+
+endmodule
